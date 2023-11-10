00dee091556ad92d221211cbf92cc7097f1ecb07
DavidSuarezHerminio <148563032+DavidSuarezHerminio@users.noreply.github.com>
2023-11-10 17:05:41 +0100

    ultimisimos cambios

--- Proyecto_Parejas/src/proyecto_parejas/Proyecto_Parejas.java
+++ Proyecto_Parejas/src/proyecto_parejas/Proyecto_Parejas.java
@@ -6,10 +6,15 @@ public class Proyecto_Parejas {
 
     public static void main(String[] args) {
         clase1_FJAG clase1=new clase1_FJAG();
+        clase2_DSM clase2 = new clase2_DSM();
         File archivo=new File("books.xml");
         
         clase1.abrirXML(archivo);
         clase1.recorrerYMostrar();
+        clase1.insertarLibroEnDOM("sdf", "sdf", "sdf");
+        clase2.borrar("sdf");
+        clase2.guardarEnOtroArchivo("sdf");
+        
     }
     
 }
